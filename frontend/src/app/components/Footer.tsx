2cd947816148d3efdad4b43863913a52c080c53e
정다니엘 <cdl2141@naver.com>
2025-07-31 19:07:59 +0900

    feat: Header, Footer 컴포넌트 생성

---
+++ frontend/src/app/components/Footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+    return (
+        <footer className="fixed bottom-0 left-0 w-[100vw] h-[60px] flex justify-center items-center">
+            <div className="w-[1440px] h-full flex flex-row justify-center items-center">
+                저작권 관련, 팀원 정보, 문의처
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
