856e2bd6d777efa2d838ce202f32ba708d623dd2
hyeok9912 <park996681@gmail.com>
2025-08-05 20:44:15 +0900

    feat : 프리티어 포맷팅

--- frontend/src/app/layoutComponents/Header.tsx
+++ frontend/src/app/layoutComponents/Header.tsx
@@ -1,27 +1,27 @@
-import Link from "next/link";
+import Link from 'next/link';
 
 const Header = () => {
-  const navArr = ["프로젝트", "포트폴리오", "팀 구하기", "기록"];
+    const navArr = ['프로젝트', '포트폴리오', '팀 구하기', '기록'];
 
-  return (
-    <header className="fixed top-0 left-0 w-[100vw] h-[60px] flex justify-center items-center text-[var(--color-gray-900)] font-semibold text-[16px] z-50 bg-white">
-      <div className="w-[1440px] h-full flex flex-row justify-between items-center">
-        <div className="w-[130px] h-[28px]">logoImage</div>
+    return (
+        <header className="fixed top-0 left-0 w-[100vw] h-[60px] flex justify-center items-center text-[var(--color-gray-900)] font-semibold text-[16px] z-50 bg-white">
+            <div className="w-[1440px] h-full flex flex-row justify-between items-center">
+                <div className="w-[130px] h-[28px]">logoImage</div>
 
-        <nav className="w-[286px] h-[36px] flex flex-row justify-between items-center">
-          {navArr.map((a, i) => (
-            <Link key={i} href={"/"} className="">
-              {a}
-            </Link>
-          ))}
-        </nav>
+                <nav className="w-[286px] h-[36px] flex flex-row justify-between items-center">
+                    {navArr.map((a, i) => (
+                        <Link key={i} href={'/'} className="">
+                            {a}
+                        </Link>
+                    ))}
+                </nav>
 
-        <div className="w-[130px] h-[24px] flex justify-end items-center">
-          <h3 className="">로그인</h3>
-        </div>
-      </div>
-    </header>
-  );
+                <div className="w-[130px] h-[24px] flex justify-end items-center">
+                    <h3 className="">로그인</h3>
+                </div>
+            </div>
+        </header>
+    );
 };
 
 export default Header;
