c0cbc810edbe8abf572dd6a4592c132033c3a1ad
정다니엘 <cdl2141@naver.com>
2025-08-19 18:28:28 +0900

    feat: slider-menu 생성 및 헤더 구조 수정

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -28,9 +28,9 @@ export default function RootLayout({
     return (
         <html lang="en">
             <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-                <Header />
-                {children}
-                <Footer />
+                    <Header />
+                    {children}
+                    <Footer />
             </body>
         </html>
     );
