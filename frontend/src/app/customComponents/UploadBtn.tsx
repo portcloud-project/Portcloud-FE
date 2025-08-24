b17aaee70b78ed4eb8d2869f226e045b852da29d
정다니엘 <cdl2141@naver.com>
2025-08-24 23:54:28 +0900

    fix: 헤더 링크 맵핑 및 업로드 버튼 페이지별 텍스트 수정

--- frontend/src/app/customComponents/UploadBtn.tsx
+++ frontend/src/app/customComponents/UploadBtn.tsx
@@ -1,10 +1,21 @@
+'use client';
+
+import { usePathname } from 'next/navigation';
 import { MdOutlineNoteAdd } from 'react-icons/md';
+
 const UploadBtn = () => {
+    const location = usePathname();
+    console.log(location);
+
     return (
         <button className="w-fit h-[48px] min-w-[193px] rounded-[100px] border border-purple-500 cursor-pointer font-semibold ">
             <div className="w-full flex justify-center items-center gap-[6px]">
                 <MdOutlineNoteAdd size={24} color="#732BF1" />
-                <p className="text-[16px] text-purple-500">포트폴리오 업로드</p>
+                {location === '/works/projects' ? (
+                    <p className="text-[16px] text-purple-500">프로젝트 업로드</p>
+                ) : (
+                    <p className="text-[16px] text-purple-500">포트폴리오 업로드</p>
+                )}
             </div>
         </button>
     );
