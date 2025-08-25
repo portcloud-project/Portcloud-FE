e021e4e486a9007e4061fd498a95355e63f0d8c5
hyeok9912 <park996681@gmail.com>
2025-08-25 19:24:53 +0900

    feat : 메인화면 데이터 패칭 및 tanstackquery 적용

---
+++ frontend/src/app/layoutComponents/QueryProvider.tsx
@@ -0,0 +1,22 @@
+'use client';
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import React, { useState } from 'react';
+
+interface QueryProviderProps {
+    children: React.ReactNode;
+}
+
+function QueryProvider({ children }: QueryProviderProps) {
+    const [queryClient] = useState(() => new QueryClient());
+
+    return (
+        <QueryClientProvider client={queryClient}>
+            {children}
+            <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
+    );
+}
+
+export default QueryProvider;
