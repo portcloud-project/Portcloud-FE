f624b05d353238a698867b06723f099bf5b87e7b
Daniel Jung <cdl2141@naver.com>
2025-09-22 19:59:34 +0900

    fix: mypage ui 틀 완

--- frontend/src/app/mypage/page.tsx
+++ frontend/src/app/mypage/page.tsx
@@ -3,8 +3,8 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 const Mypage = () => {
     const tabsArr = [
         { value: 'bookMarks', title: '북마크 관리' },
-        { value: 'projects', title: '프로젝트 관리' },
         { value: 'portfolios', title: '포트폴리오 관리' },
+        { value: 'projects', title: '프로젝트 관리' },
         { value: 'teams', title: '팀 관리' },
         { value: 'myActivity', title: '내 활동' },
         { value: 'settings', title: '설정' },
@@ -16,10 +16,19 @@ const Mypage = () => {
                 defaultValue="profile"
                 className="w-[1392px] h-[764px] border border-[var(--color-gray-300)]"
             >
-                <TabsList className="">
-                    <TabsTrigger value="profile" className="w-full h-[344px]">
-                        프로필 수정
-                    </TabsTrigger>
+                <TabsList>
+                    <div className='flex flex-col justify-center items-center gap-[24px] w-full h-[344px] py-[48px] border-b-[1px] border-[var(--color-gray-300)]'>
+                        {/* 프로필 사진 */}
+                        <div className='w-[92px] h-[92px] rounded-full border border-black'></div>
+                        {/* 닉네임, 이메일 */}
+                        <div className='flex flex-col justify-center items-center gap-[4px]'>
+                            <h3 className='text-balck text-[28px] font-bold'>닉네임</h3>
+                            <h3 className='text-[var(--color-gray-500)] font-semibold text-[16px]'>이메일</h3>
+                        </div>
+                        <TabsTrigger value="profile" className="w-[121px] h-[40px] flex justify-center items-center text-[16px] font-semibold text-white bg-[var(--color-purple-500)] rounded-[20px]">
+                            프로필 수정
+                        </TabsTrigger>
+                    </div>
                     <div className="p-[12px]">
                         {tabsArr.map((a, i) => {
                             return (
@@ -31,11 +40,28 @@ const Mypage = () => {
                     </div>
                 </TabsList>
 
-                {tabsArr.map((a, i) => (
-                    <TabsContent key={i} value={a.value}>
-                        {a.value}
-                    </TabsContent>
-                ))}
+                <TabsContent value='profile'>
+                    여긴 프로필 수정
+                </TabsContent>
+                <TabsContent value='bookMarks'>
+                    여긴 북마크 관리
+                </TabsContent>
+                <TabsContent value='portfolios'>
+                    여긴 포트폴리오
+                </TabsContent>
+                <TabsContent value='projects'>
+                    여긴 프로젝트
+                </TabsContent>
+                <TabsContent value='teams'>
+                    여긴 팀관리
+                </TabsContent>
+                <TabsContent value='myActivity'>
+                    여긴 내 활동
+                </TabsContent>
+                <TabsContent value='settings'>
+                    여긴 설정
+                </TabsContent>
+                
             </Tabs>
         </main>
     );
