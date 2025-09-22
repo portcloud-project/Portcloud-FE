15ad87c037e9b7402e238ba0c0326f67a006a327
dann0316 <88477755+dann0316@users.noreply.github.com>
2025-09-22 15:11:32 +0900

    Fix/project upload (#65)
    * fix: 이메일 인증 시스템 완, 회월 가입 완
    
    * fix: format 수정
    
    * fix: eslint 오류 수정
    
    * fix: lint 오류 수정
    
    * fix: poject-upload 페이지 api 연결
    
    * feat: 마이페이지 틀 생성
    
    * fix: format check 수정

---
+++ frontend/src/components/ui/tabs.tsx
@@ -0,0 +1,51 @@
+'use client';
+
+import * as React from 'react';
+import * as TabsPrimitive from '@radix-ui/react-tabs';
+
+import { cn } from '@/lib/utils';
+
+function Tabs({ className, ...props }: React.ComponentProps<typeof TabsPrimitive.Root>) {
+    return (
+        <TabsPrimitive.Root
+            data-slot="tabs"
+            className={cn('flex flex-row', className)}
+            {...props}
+        />
+    );
+}
+
+function TabsList({ className, ...props }: React.ComponentProps<typeof TabsPrimitive.List>) {
+    return (
+        <TabsPrimitive.List
+            data-slot="tabs-list"
+            className={cn('flex flex-col w-[302px] border border-black', className)}
+            {...props}
+        />
+    );
+}
+
+function TabsTrigger({ className, ...props }: React.ComponentProps<typeof TabsPrimitive.Trigger>) {
+    return (
+        <TabsPrimitive.Trigger
+            data-slot="tabs-trigger"
+            className={cn(
+                "data-[state=active]:bg-background dark:data-[state=active]:text-foreground focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:outline-ring dark:data-[state=active]:border-input dark:data-[state=active]:bg-input/30  dark:text-muted-foreground inline-flex  items-center justify-start gap-1.5 px-[24px] py-1 whitespace-nowrap transition-[color,box-shadow] focus-visible:ring-[3px] focus-visible:outline-1 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:shadow-sm [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4 border-t-[1px] border-[var(--color-gray-300)] w-full h-[68px] text-[20px] text-[var(--color-gray-600)] font-medium cursor-pointer hover:text-[var(--color-purple-400)] data-[state=active]:text-[var(--color-purple-400)]",
+                className,
+            )}
+            {...props}
+        />
+    );
+}
+
+function TabsContent({ className, ...props }: React.ComponentProps<typeof TabsPrimitive.Content>) {
+    return (
+        <TabsPrimitive.Content
+            data-slot="tabs-content"
+            className={cn('flex-1 outline-none', className)}
+            {...props}
+        />
+    );
+}
+
+export { Tabs, TabsList, TabsTrigger, TabsContent };
