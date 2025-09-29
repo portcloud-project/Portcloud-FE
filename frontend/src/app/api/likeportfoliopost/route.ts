61d0fb161e2d32012cddfc83df6df2a33b35c88f
hyeok9912 <park996681@gmail.com>
2025-09-29 19:29:40 +0900

    fix: eslint

--- frontend/src/app/api/likeportfoliopost/route.ts
+++ frontend/src/app/api/likeportfoliopost/route.ts
@@ -20,6 +20,7 @@ export async function POST(request: NextRequest) {
         );
         return NextResponse.json(response.data);
     } catch (err) {
+        console.error(err);
         throw err;
     }
 }
