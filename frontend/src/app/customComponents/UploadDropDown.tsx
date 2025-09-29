e4344c997f19fbfff47f1d33c9eb530f52b043b0
hyeok9912 <park996681@gmail.com>
2025-09-29 19:27:00 +0900

    feat : team

--- frontend/src/app/customComponents/UploadDropDown.tsx
+++ frontend/src/app/customComponents/UploadDropDown.tsx
@@ -7,6 +7,7 @@ import {
     DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
 import { useState } from 'react';
+import { Controller, useFormContext } from 'react-hook-form';
 import { FaAngleDown, FaAngleUp, FaCheck } from 'react-icons/fa6';
 
 type UploadDropDownPropsType = {
@@ -19,23 +20,24 @@ type UploadDropDownPropsType = {
     labelText: string;
     labelFont: string;
     gap: string;
+    name: string;
+
     // eslint-disable-next-line no-unused-vars
     onChange?: (a: string) => void;
 };
-
 const UploadDropDown = ({
     arr,
     dropDownLabel,
     dropDownPlaceholoder,
     width,
     height,
-    value,
-    onChange,
+    name,
     gap,
     labelText,
     labelFont,
 }: UploadDropDownPropsType) => {
     const [open, setOpen] = useState<boolean>(false);
+    const { control } = useFormContext();
 
     return (
         <div className={`w-fit flex flex-col justify-between items-start ${gap}`}>
@@ -45,46 +47,50 @@ const UploadDropDown = ({
             >
                 {dropDownLabel}
             </label>
-
-            <DropdownMenu open={open} onOpenChange={setOpen}>
-                <DropdownMenuTrigger
-                    className={` ${width} ${height} border rounded-[8px] px-[12px] flex items-center justify-between cursor-pointer transition duration-300 ease-in-out
-  ${open ? 'border-[var(--color-purple-500)]' : 'border-[var(--color-gray-400)]'}`}
-                >
-                    <span className="flex-1 text-left">
-                        {value || (
-                            <span className="text-[var(--color-gray-600)] text-[16px]">
-                                {dropDownPlaceholoder}
-                            </span>
-                        )}
-                    </span>
-                    <span className="ml-2 flex-shrink-0">
-                        {open ? (
-                            <FaAngleUp className="w-[15px] h-[15px] text-[var(--color-gray-400)]" />
-                        ) : (
-                            <FaAngleDown className="w-[15px] h-[15px] text-[var(--color-gray-400)]" />
-                        )}
-                    </span>
-                </DropdownMenuTrigger>
-                <DropdownMenuContent
-                    className={`${width} flex flex-col gap-[4px] p-[6px]`}
-                    side="bottom"
-                    align="start"
-                    sideOffset={6}
-                    alignOffset={0}
-                >
-                    {arr.map((a, i) => (
-                        <DropdownMenuItem
-                            key={i}
-                            className="text-[16px] text-[var(--color-gray-700)] data-[highlighted]:text-white bg-[white] data-[highlighted]:bg-[var(--color-purple-500)] transition duration-300 ease-in-out w-[364px] h-[48px]"
-                            onSelect={() => onChange?.(a)}
+            <Controller
+                control={control}
+                name={`${name}`}
+                render={({ field }) => (
+                    <DropdownMenu open={open} onOpenChange={setOpen}>
+                        <DropdownMenuTrigger
+                            className={` ${width} ${height} border rounded-[8px] px-[12px] flex items-center justify-between cursor-pointer transition duration-300 ease-in-out ${open ? 'border-[var(--color-purple-500)]' : 'border-[var(--color-gray-400)]'}`}
                         >
-                            <FaCheck className="text-white" />
-                            {a}
-                        </DropdownMenuItem>
-                    ))}
-                </DropdownMenuContent>
-            </DropdownMenu>
+                            <span className="flex-1 text-left">
+                                {field.value || (
+                                    <span className="text-[var(--color-gray-600)] text-[16px]">
+                                        {dropDownPlaceholoder}
+                                    </span>
+                                )}
+                            </span>
+                            <span className="ml-2 flex-shrink-0">
+                                {open ? (
+                                    <FaAngleUp className="w-[15px] h-[15px] text-[var(--color-gray-400)]" />
+                                ) : (
+                                    <FaAngleDown className="w-[15px] h-[15px] text-[var(--color-gray-400)]" />
+                                )}
+                            </span>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent
+                            className={`${width} flex flex-col gap-[4px] p-[6px]`}
+                            side="bottom"
+                            align="start"
+                            sideOffset={6}
+                            alignOffset={0}
+                        >
+                            {arr.map((a, i) => (
+                                <DropdownMenuItem
+                                    key={i}
+                                    className="text-[16px] text-[var(--color-gray-700)] data-[highlighted]:text-white bg-[white] data-[highlighted]:bg-[var(--color-purple-500)] transition duration-300 ease-in-out w-[364px] h-[48px]"
+                                    onSelect={() => field.onChange?.(a)}
+                                >
+                                    <FaCheck className="text-white" />
+                                    {a}
+                                </DropdownMenuItem>
+                            ))}
+                        </DropdownMenuContent>
+                    </DropdownMenu>
+                )}
+            />
         </div>
     );
 };
