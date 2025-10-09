55af49e1a3eecab5f88315d31124fd15776166c3
hyeok9912 <park996681@gmail.com>
2025-10-10 05:15:47 +0900

    feat : logs

--- frontend/src/app/output/logs/[id]/edit/page.tsx
+++ frontend/src/app/output/logs/[id]/edit/page.tsx
@@ -4,34 +4,21 @@ import CommentViewLogs from '@/app/customComponents/CommentViewLogs';
 import Like from '@/app/customComponents/Like';
 import LikePostLogs from '@/app/customComponents/LikePostLogs';
 import TopBtn from '@/app/customComponents/TopBtn';
-import { useDeleteLogs } from '@/app/hooks/useDeleteAllLogs';
 import { useLikeLogs } from '@/app/hooks/useLikeLogs';
 import { useLogsDetail } from '@/app/hooks/useLogsDetail';
 import dayjs from 'dayjs';
-import { useParams, useRouter } from 'next/navigation';
+import { useParams } from 'next/navigation';
 import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
 
-const LogsOutput = () => {
+const LogsEdit = () => {
     const params = useParams();
-    const router = useRouter();
     const id = params.id;
     const { data: logs, isLoading, isError, error } = useLogsDetail(id);
     const { data: like } = useLikeLogs(id);
-    console.log(logs?.id);
-    const deleteMutation = useDeleteLogs();
     if (isLoading) return <p>불러오는 중...</p>;
     if (isError) return <p className="text-red-500">에러 발생 {error.message}</p>;
     if (!logs?.id) return <p>기록이 삭제되었거나 찾을 수 없습니다.</p>;
-
-    const handleDelete = async () => {
-        if (!confirm('정말 삭제하시겠습니까?')) return;
-        try {
-            await deleteMutation.mutateAsync(id);
-        } catch (err) {
-            console.error(err);
-        }
-    };
 
     return (
         <main className="w-[768px]  flex  justify-start items-center ">
@@ -52,15 +39,6 @@ const LogsOutput = () => {
                             <p className=" text-purple-500">카테고리</p>
                             <p className="text-purple-500 font-semibold">{logs?.category}</p>
                         </div>
-                        {logs?.owner && (
-                            <div className="flex w-fit items-center gap-[12px]">
-                                <button onClick={() => router.push(`/output/logs/${id}/edit`)}>
-                                    수정
-                                </button>
-                                <div className="border-r h-[14px] border-gray-300" />
-                                <button onClick={handleDelete}>삭제</button>
-                            </div>
-                        )}
                     </div>
                     <hr />
                 </section>
@@ -77,4 +55,4 @@ const LogsOutput = () => {
         </main>
     );
 };
-export default LogsOutput;
+export default LogsEdit;
