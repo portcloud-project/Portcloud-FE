887defed80a1485598f5982924da4a3ee407d467
Daniel Jung <cdl2141@naver.com>
2025-10-19 01:41:07 +0900

    fix: MypageTeam eslint error fix

--- frontend/src/app/customComponents/MypageTeam.tsx
+++ frontend/src/app/customComponents/MypageTeam.tsx
@@ -1,51 +1,50 @@
-/* eslint-disable no-unused-vars */
 'use client';
 
-import dayjs from 'dayjs';
-import LoadingSpinner from './LoadingSpinner';
-// import MypageAdd from './MypageAdd';
-import { FaTrashCan } from 'react-icons/fa6';
-import { useRouter } from 'next/navigation';
-import { useQueryClient } from '@tanstack/react-query';
-import { useAllTeam } from '../hooks/useAllTeam';
-import { useDeleteTeam } from '../hooks/useDeleteTeam';
+// import dayjs from 'dayjs';
+// import LoadingSpinner from './LoadingSpinner';
+// // import MypageAdd from './MypageAdd';
+// // import { FaTrashCan } from 'react-icons/fa6';
+// import { useRouter } from 'next/navigation';
+// import { useQueryClient } from '@tanstack/react-query';
+// import { useAllTeam } from '../hooks/useAllTeam';
+// import { useDeleteTeam } from '../hooks/useDeleteTeam';
 
 const MypageTeam = () => {
-    const { data: items, isLoading, isError, error } = useAllTeam();
-    const deleteMutation = useDeleteTeam();
-    const router = useRouter();
-    const queryClient = useQueryClient();
+    // const { data: items, isLoading, isError, error } = useAllTeam();
+    // const deleteMutation = useDeleteTeam();
+    // const router = useRouter();
+    // const queryClient = useQueryClient();
 
-    if (items?.length === 0) {
-        return <div>모집하고 있는 팀이 존재하지 않습니다</div>;
-    }
-    if (isLoading) {
-        return (
-            <div className="flex items-center justify-center w-full h-[50vh]">
-                <LoadingSpinner />
-            </div>
-        );
-    }
-    if (isError) {
-        return (
-            <p className="text-red-500 flex items-center justify-center w-full h-[50vh]">
-                {error instanceof Error ? error.message : '데이터를 불러오지 못했습니다.'}
-            </p>
-        );
-    }
-    const handleDelete = async (id: string) => {
-        if (!confirm('정말 삭제하시겠습니까?')) return;
-        try {
-            await deleteMutation.mutateAsync(id);
-            await queryClient.invalidateQueries({ queryKey: ['allportfolio'] });
-        } catch (err) {
-            console.error(err);
-        }
-    };
+    // if (items?.length === 0) {
+    //     return <div>모집하고 있는 팀이 존재하지 않습니다</div>;
+    // }
+    // if (isLoading) {
+    //     return (
+    //         <div className="flex items-center justify-center w-full h-[50vh]">
+    //             <LoadingSpinner />
+    //         </div>
+    //     );
+    // }
+    // if (isError) {
+    //     return (
+    //         <p className="text-red-500 flex items-center justify-center w-full h-[50vh]">
+    //             {error instanceof Error ? error.message : '데이터를 불러오지 못했습니다.'}
+    //         </p>
+    //     );
+    // }
+    // const handleDelete = async (id: string) => {
+    //     if (!confirm('정말 삭제하시겠습니까?')) return;
+    //     try {
+    //         await deleteMutation.mutateAsync(id);
+    //         await queryClient.invalidateQueries({ queryKey: ['allportfolio'] });
+    //     } catch (err) {
+    //         console.error(err);
+    //     }
+    // };
 
     return (
         <div className="flex flex-wrap gap-[12px]">
-            {items.map((a, idx) => {
+            {/* {items.map((a, idx) => {
                 return (
                     <li
                         key={`${a}_${idx}`}
@@ -73,7 +72,7 @@ const MypageTeam = () => {
                         <div className="">{a?.writerName}</div>
                     </li>
                 );
-            })}
+            })} */}
         </div>
     );
 };
