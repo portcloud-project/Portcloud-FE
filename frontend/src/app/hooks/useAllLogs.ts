6e3b7008eab1f0f0f1d6dc25d4cb12b77f85fa25
hyeok9912 <park996681@gmail.com>
2025-10-20 14:42:02 +0900

    fix : search result edit

--- frontend/src/app/hooks/useAllLogs.ts
+++ frontend/src/app/hooks/useAllLogs.ts
@@ -13,11 +13,18 @@ interface AllLogs {
 }
 
 const fetchAllLogs = async (): Promise<AllLogs[]> => {
-    const response = await axios.get<ApiResponse<AllLogs[]>>('/api/allLogs');
-    if (response.data && Array.isArray(response.data.data)) {
-        return response.data.data;
-    } else {
-        throw new Error('API 응답 형식이 올바르지 않습니다.');
+    try {
+        const response = await axios.get<ApiResponse<AllLogs[]>>('/api/allLogs');
+        if (response.data && Array.isArray(response.data.data)) {
+            return response.data.data;
+        } else {
+            throw new Error('API 응답 형식이 올바르지 않습니다.');
+        }
+    } catch (error) {
+        if (axios.isAxiosError(error) && error.response?.status === 404) {
+            throw new Error('기록이 존재하지 않습니다.');
+        }
+        throw error;
     }
 };
 
--- frontend/src/app/works/logs/page.tsx
+++ frontend/src/app/works/logs/page.tsx
@@ -18,7 +18,7 @@ const Logs = () => {
         threshold: 0.5,
         triggerOnce: false,
     });
-    const { data, fetchNextPage, hasNextPage, isFetchingNextPage } = useRecentLogs();
+    const { data, fetchNextPage, hasNextPage, isLoading, isFetchingNextPage } = useRecentLogs();
 
     useEffect(() => {
         if (inView && hasNextPage && !isFetchingNextPage) {
@@ -35,39 +35,56 @@ const Logs = () => {
     });
     return (
         <main className="w-full px-[24px] flex flex-col justify-start items-start mx-auto laptop:max-w-[1440px] tablet:w-full gap-[48px]">
-            <Tabs className="flex flex-col  w-full gap-[48px]" defaultValue="my-logs">
-                <TabsList className="flex gap-[12px] items-center">
-                    <TabsTrigger
-                        value="my-logs"
-                        className="cursor-pointer text-gray-500  data-[state=active]:text-black data-[state=active]:font-bold "
-                    >
-                        내 기록
-                    </TabsTrigger>
-                    <div className="border-r h-[14px] border-gray-300" />
-                    <TabsTrigger
-                        value="like-logs"
-                        className="cursor-pointer text-gray-500  data-[state=active]:text-black data-[state=active]:font-bold"
-                    >
-                        추천기록
-                    </TabsTrigger>
-                    <div className="border-r h-[14px] border-gray-300" />
-                    <TabsTrigger
-                        value="recent-logs"
-                        className="cursor-pointer text-gray-500  data-[state=active]:text-black data-[state=active]:font-bold"
-                    >
-                        최신기록
-                    </TabsTrigger>
-                </TabsList>
-                <TabsContent value="recent-logs">
-                    <RecentLogs contentWithItem={filteredItems} />
-                </TabsContent>
-                <TabsContent value="like-logs">
-                    <MainLogs />
-                </TabsContent>
-                <TabsContent value="my-logs">
-                    <MyPageLogs />
-                </TabsContent>
-            </Tabs>
+            {!searchTitle && (
+                <Tabs className="flex flex-col  w-full gap-[48px]" defaultValue="my-logs">
+                    <TabsList className="flex gap-[12px] items-center">
+                        <TabsTrigger
+                            value="my-logs"
+                            className="cursor-pointer text-gray-500  data-[state=active]:text-black data-[state=active]:font-bold "
+                        >
+                            내 기록
+                        </TabsTrigger>
+                        <div className="border-r h-[14px] border-gray-300" />
+                        <TabsTrigger
+                            value="like-logs"
+                            className="cursor-pointer text-gray-500  data-[state=active]:text-black data-[state=active]:font-bold"
+                        >
+                            추천기록
+                        </TabsTrigger>
+                        <div className="border-r h-[14px] border-gray-300" />
+                        <TabsTrigger
+                            value="recent-logs"
+                            className="cursor-pointer text-gray-500  data-[state=active]:text-black data-[state=active]:font-bold"
+                        >
+                            최신기록
+                        </TabsTrigger>
+                    </TabsList>
+                    <TabsContent value="recent-logs">
+                        <RecentLogs contentWithItem={filteredItems} />
+                    </TabsContent>
+                    <TabsContent value="like-logs">
+                        <MainLogs />
+                    </TabsContent>
+                    <TabsContent value="my-logs">
+                        <MyPageLogs />
+                    </TabsContent>
+                </Tabs>
+            )}
+            {searchTitle && (
+                <div className="w-full flex flex-col gap-[16px]">
+                    <div className="flex gap-2">
+                        <div className="text-purple-500 font-bold">{searchTitle}</div>
+                        <div className="font-semibold">에 대한 검색 결과</div>
+                    </div>
+                    <RecentLogs title={''} contentWithItem={filteredItems} />
+                </div>
+            )}
+            {!isLoading && filteredItems.length === 0 && (
+                <div className="flex flex-col justify-center items-center absolute top-[40vh] left-[50%] -translate-x-1/2 gap-[24px]">
+                    <div className="text-[40px] font-bold">아직 기록이 등록되지 않았어요!</div>
+                    <div className="text-[28px] font-bold">{searchTitle}에 대한 검색 결과 0건</div>
+                </div>
+            )}
 
             <div ref={ref}>{isFetchingNextPage && <p>데이터를 받아오는 중....</p>}</div>
         </main>
